6c722289e4d551b4619f00f1cc53a4f512717f2c
unknown <kosiaroh@gmail.com>
2018-07-25 03:07:37 -0400

    add accordion

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import Banner from '../components/Banner.jsx';
 import {withTracker} from 'meteor/react-meteor-data';
-import Accordion from 'react-responsive-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemTitle,
+    AccordionItemBody,
+} from 'react-accessible-accordion';
 
 function Home() {
   return (
@@ -25,18 +31,24 @@ function Home() {
       </ul>
 
       <Accordion>
-          <div data-trigger="A nifty React accordion component">
-            <p>So this is an Accordion component that used the <a href="https://github.com/glennflanagan/react-collapsible">react-collapsible</a> component. How handy.</p>
-          </div>
- 
-          <div data-trigger="What the difference?" data-trigger-when-open="THAT is the difference!">
-            <p>An Accordion is different to a Collapsible in the sense that only one "tray" will be open at any one time.</p>
-          </div>
- 
-          <div data-trigger="I'm responsive and I have a little secret. Look inside.">
-            <p>And this Accordion component is also completely repsonsive. Hurrah for mobile users!</p>
-          </div>
-        </Accordion>
+        <AccordionItem>
+            <AccordionItemTitle>
+                <h3>Simple title</h3>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+                <p>Body content</p>
+            </AccordionItemBody>
+        </AccordionItem>
+        <AccordionItem>
+            <AccordionItemTitle>
+                <h3>Complex title</h3>
+                <div>With a bit of description</div>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+                <p>Body content</p>
+            </AccordionItemBody>
+        </AccordionItem>
+    </Accordion>
 
     </div>
   );
